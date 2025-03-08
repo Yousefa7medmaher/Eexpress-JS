ccdaeb3cdaed9d356d5e529b8c2f330bc5fdc218
JooAhmed <160799433+Yousefa7medmaher@users.noreply.github.com>
2025-03-08 07:40:07 +0200

    Update routes.js to add DeleteTask rout

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,6 +1,6 @@
 import express from 'express' ;
 const route = express.Router();
-import {AddTask , UpdateTask , GetTaskById , GetAllTasks , CompleteTask} from '../Controller/CRUD.js' ;
+import {AddTask , UpdateTask , GetTaskById , GetAllTasks , CompleteTask, DeleteTask} from '../Controller/CRUD.js' ;
 
 
 route.route('/addtask').post(AddTask); 
@@ -8,4 +8,5 @@ route.route('/UpdateTask').put(UpdateTask);
 route.route('/CompleteTask').put(CompleteTask); 
 route.route('/GetTaskById/:id').get(GetTaskById); 
 route.route('/GetAllTasks').get(GetAllTasks); 
+route.route('/DeleteTask').delete(DeleteTask);  
 export default route;
